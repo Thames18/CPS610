ee14dab47366e0f9b3329fab7d0620c5572651c9
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-03-29 19:35:15 -0400

    Update home.php

--- home.php
+++ home.php
@@ -4,6 +4,18 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
 
+  <?php
+$servername = "webdev.scs.ryerson.ca";
+$username = "malsalem";
+$password = "notKiWaj ";
+// Create connection
+$conn = mysqli_connect("localhost", "malsalem", "notKiWaj", "malsalem");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+  
 <div class="w3-sidebar w3-light-grey w3-bar-block w3-card-2" style="width:25%">
   <h3 class="w3-bar-item w3-black">
       <a href="home.php" class="w3-bar-item w3-black">CCPS 610 Project 1B</a>
