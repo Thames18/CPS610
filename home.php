68c159a98e699353388c12dad9711f632e705ff2
InverseFlash <jstos283@gmail.com>
2020-03-29 20:18:15 -0400

    Updated for CCPS610 Assignment.  Added TODO's for linking appropriate php files.

--- home.php
+++ home.php
@@ -3,33 +3,34 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
+<div class="w3-sidebar w3-light-grey w3-bar-block w3-card-2" style="width:30%">
+    <h3 class="w3-bar-item w3-black">
+        <a href="home.php" class="w3-bar-item w3-black">CCPS 610 Assignment</a>
+    </h3>
 
-<div class="w3-sidebar w3-light-grey w3-bar-block w3-card-2" style="width:25%">
-  <h3 class="w3-bar-item w3-black">
-      <a href="home.php" class="w3-bar-item w3-black">CCPS 610 Project 1B</a>
-  </h3>
+    <!-- Task 1 Related Activities-->
+    <!--TODO: link buttons to appropriate files-->
+    <div class="w3-blue w3-card-2 w3-container">
+        <p>Employee Main Menu</p>
+    </div>
+    <a href="addActorDirector.php" class="w3-bar-item w3-button">Employee Hiring Form</a>
+    <a href="****.php" class="w3-bar-item w3-button">Update Employee Records</a>
 
-  <div class="w3-blue w3-card-2 w3-container">
-    <p>Input New Information</p>
-  </div>
+    <!-- Task 2 Related Activities-->
+    <!--TODO: link buttons to appropriate files-->
+    <div class="w3-container w3-green w3-card-2">
+        <p>Jobs Main Menu</p>
+    </div>
+    <a href="actorInfo.php" class="w3-bar-item w3-button">Identify Job Description</a>
+    <a href="movieInfo.php" class="w3-bar-item w3-button">Change Job Description</a>
+    <a href="****.php" class="w3-bar-item w3-button">Create New Job</a>
 
-  <a href="addActorDirector.php" class="w3-bar-item w3-button">Add Person</a>
-  <a href="addMovie.php" class="w3-bar-item w3-button">Add Movie</a>
-  <a href="addMovieActor.php" class="w3-bar-item w3-button">Add Movie Actor</a>
-  <a href="addMovieDirector.php" class="w3-bar-item w3-button">Add Movie Director</a>
-
-  <div class="w3-container w3-green w3-card-2">
-    <p>Browse Information</p>
-  </div>
-
-  <a href="actorInfo.php" class="w3-bar-item w3-button">Actor Information</a>
-  <a href="movieInfo.php" class="w3-bar-item w3-button">Movie Information</a>
-
-  <div class="w3-container w3-red w3-card-2">
-    <p>Search</p>
-  </div>
-
-  <a href="search.php" class="w3-bar-item w3-button">Search For Actor/Movie</a>
+    <!-- Task 3 Related Activities-->
+    <!--TODO: link buttons to appropriate files-->
+    <div class="w3-container w3-red w3-card-2">
+        <p>Departments Main Menu</p>
+    </div>
+    <a href="search.php" class="w3-bar-item w3-button">Verify Salary Range</a>
 
 </div>
 </body>
