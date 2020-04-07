0dcb4b7a1e600a5b15ab486164f1ab7dd940e21d
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-04-06 22:51:11 -0400

    Update insert2.php

--- insert2.php
+++ insert2.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "test");
+$link = mysqli_connect("localhost", "root", "admin", "hr_schema");
  
 // Check connection
 if($link === false){
