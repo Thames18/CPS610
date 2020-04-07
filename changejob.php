2685b7904d8a87dcf585041382542ed1455fcd54
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-04-06 22:49:06 -0400

    Update changejob.php

--- changejob.php
+++ changejob.php
@@ -12,24 +12,24 @@
     <div class="w3-blue w3-card-2 w3-container">
         <p>Employee Main Menu</p>
     </div>
-    <a href="employee_hiring_form.php" class="w3-bar-item w3-button">Employee Hiring Form</a>
-    <a href="update.php" class="w3-bar-item w3-button">Update Employee Records</a>
+    <a href="employee_hiring_form.php" class="w3-bar-item w3-button">&emsp;Employee Hiring Form</a>
+    <a href="update2.php" class="w3-bar-item w3-button">&emsp;Update Employee Records</a>
 
     <!-- Task 2 Related Activities-->
     <!--TODO: link buttons to appropriate files-->
     <div class="w3-container w3-green w3-card-2">
         <p>Jobs Main Menu</p>
     </div>
-    <a href="getjob.php" class="w3-bar-item w3-button">Identify Job Description</a>
-    <a href="changejob.php" class="w3-bar-item w3-button">Change Job Description</a>
-    <a href="****.php" class="w3-bar-item w3-button">Create New Job</a>
+    <a href="getjob.php" class="w3-bar-item w3-button">&emsp;Identify Job Description</a>
+    <a href="changejob.php" class="w3-bar-item w3-button">&emsp;Change Job Description</a>
+    <a href="create.php" class="w3-bar-item w3-button">&emsp;Create New Job</a>
 
     <!-- Task 3 Related Activities-->
     <!--TODO: link buttons to appropriate files-->
     <div class="w3-container w3-red w3-card-2">
         <p>Departments Main Menu</p>
     </div>
-    <a href="****.php" class="w3-bar-item w3-button">Verify Salary Range</a>
+    <a href="****.php" class="w3-bar-item w3-button">&emsp;Verify Salary Range</a>
 
 </div>
 
