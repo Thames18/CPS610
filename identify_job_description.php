0079dfbe4a0956a8c9f100a5c10cc13be7052dee
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-04-08 15:24:12 -0400

    Update identify_job_description.php

--- identify_job_description.php
+++ identify_job_description.php
@@ -3,6 +3,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
+        <style>
+
+/* Style the active class, and buttons on mouse-over */
+.active, .btn:hover {
+  background-color: #D89327;
+  color: white;
+}
+</style>
 <div class="w3-sidebar w3-light-grey w3-bar-block w3-card-2" style="width:25%">
     <h3 class="w3-bar-item w3-black">
         <a href="index.php" class="w3-bar-item w3-black">CCPS 610 Assignment</a>
@@ -20,7 +28,7 @@
     <div class="w3-container w3-green w3-card-2">
         <p>Jobs Main Menu</p>
     </div>
-    <a href="identify_job_description.php" class="w3-bar-item w3-button">Identify Job Description</a>
+    <a href="identify_job_description.php" class="w3-bar-item w3-button active">Identify Job Description</a>
     <a href="change_job_description.php" class="w3-bar-item w3-button">Change Job Description</a>
     <a href="create_new_job.php" class="w3-bar-item w3-button">Create New Job</a>
 
